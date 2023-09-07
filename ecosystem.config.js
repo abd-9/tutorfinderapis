b756506d8d3c4af1ee9d0af56a9cf0d544da616b
AbdAlkareem <abd.alkareem.ataya@gmail.com>
2023-09-07 21:15:57 +0400

    env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,8 +7,8 @@
 module.exports = {
   apps: [
     {
-      name: 'prod', // pm2 start App name
-      script: 'dist/server.js',
+      name: 'production', // pm2 start App name
+      script: 'dist/index.js',
       exec_mode: 'cluster', // 'cluster' or 'fork'
       instance_var: 'INSTANCE_ID', // instance variable
       instances: 2, // pm2 instance count
